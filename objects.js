cb4ef35669099bffd4697fff2d68071d82885fd5
kind-prototype-8380 <dk2800@nyu.edu>
2020-08-11 17:11:53 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,5 @@
+var playlist = {artistName: "songTitle"};
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  return object.assign({}, playlist, {[Phil Ochs]: "My Bloody Valentine"})
+}
