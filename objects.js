b8401f6d804b6030fe735af64da6e43bbd9360f8
kind-prototype-8380 <dk2800@nyu.edu>
2020-08-11 17:21:40 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = {artistName: "songTitle"};
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.slowdive = "My Bloody Valentine"
+  playlist.philOchs= "My Bloody Valentine"
 }
