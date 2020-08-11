71f069c49a435bf83aa3f7086981cf093140b94f
kind-prototype-8380 <dk2800@nyu.edu>
2020-08-11 17:27:58 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,3 +4,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
  return playlist [artistName, "My Bloody Valentine", "Phil Ochs", "Slowdive"];
 }
 
+playlist("My Bloody Valentine");
+playlist("Phil Ochs");
+playlist("Slowdive")
