dec237bf0d4a0503beba33709649f7b22219d826
kind-prototype-8380 <dk2800@nyu.edu>
2020-08-11 17:58:04 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -7,8 +7,8 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist[myBloodyValentine];
-  delete playlist[philOchs];
-  delete playlist[slowdive];
+  delete playlist["My Bloody Valentine"];
+  delete playlist["Phil Ochs"];
+  delete playlist["Slowdive"];
 
 }
